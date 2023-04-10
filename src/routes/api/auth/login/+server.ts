5c22046adecb22f18e2246fb353a0b1956e8de3e
GrungeElFz <elf0114_@hotmail.com>
2023-04-10 01:30:07 -0500

    Refactored:   Improved a const variable `scope` for the readability and maintainability (src/routes/api/auth/login/+server.ts)               ↳ Replaced single-quotes with back-ticks               ↳ Added a `.trim` method
                  /* remarks: The method removes leading and trailing whitespace from a string.
                  /* returns: A new trimmed string, without modifying the original string.

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -26,8 +26,27 @@ const generateRandomString = (length: number) => {
 
 const state = generateRandomString(16);
 const challenge = pkce.create();
-const scope =
-	'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read';
+const scope = `
+  ugc-image-upload 
+  user-read-playback-state 
+  user-modify-playback-state 
+  user-read-currently-playing 
+  app-remote-control 
+  streaming 
+  playlist-read-private 
+  playlist-read-collaborative 
+  playlist-modify-private 
+  playlist-modify-public 
+  user-follow-modify 
+  user-follow-read 
+  user-read-playback-position 
+  user-top-read 
+  user-read-recently-played 
+  user-library-modify 
+  user-library-read 
+  user-read-email 
+  user-read-private
+`.trim();
 
 /**
  * Redirects the user to the Spotify authorization page.
