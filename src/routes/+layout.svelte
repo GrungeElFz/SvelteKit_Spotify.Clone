b31efb26c1f3d93033181fd0f4f8b149d6de1f4f
GrungeElFz <elf0114_@hotmail.com>
2023-04-11 17:37:21 -0500

    Modified: Added a logged-in username display (src/routes/+layout.svelte)
              Import type { LayoutData } from './$types'
              Export let data: LayoutData
              Added a reactive variable `$: user = data.user`
    
              Added an {#if user} block
                ↳ <p>Hi, {user.display_name}</p>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
+	$: user = data.user;
 </script>
+
+{#if user}
+	<p>Hi, {user.display_name}</p>
+{/if}
 
 <slot />
