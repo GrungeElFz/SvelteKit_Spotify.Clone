f488469c1642ac33726d8a999f63e0612f69541d
GrungeElFz <elf0114_@hotmail.com>
2023-04-11 23:48:58 -0500

    Refactored:   Replaced a `<form>` element with `<LogoutButton />` component (src/routes/+layout.svelte)
    Added:    Migrated the `<form>` element to this component (src/lib/components/LogoutButton.svelte)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
-	import { invalidateAll } from '$app/navigation';
+	import LogoutButton from '../lib/components/LogoutButton.svelte';
 
 	export let data: LayoutData;
 
@@ -11,24 +11,7 @@
 
 {#if user}
 	<p>Hi, {user.display_name}</p>
-	<form
-		method="POST"
-		action="/api/auth/logout"
-		on:submit|preventDefault={async () => {
-			const response = await fetch('/api/auth/logout', {
-				method: 'POST',
-				headers: {
-					accept: 'application/json'
-				}
-			});
-
-			if (response.ok) {
-				invalidateAll();
-			}
-		}}
-	>
-		<button type="submit">Logout</button>
-	</form>
+	<LogoutButton />
 {/if}
 
 <slot />
