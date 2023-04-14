915dc9ba18a46d5f1dcf306b0113b9c79faf57d8
GrungeElFz <elf0114_@hotmail.com>
2023-04-14 16:39:18 -0500

    Added:    A `Navigation` component (src/lib/components/Navigation.svelte)
              Import { Home, Search, ListMusic, type Icon } from 'lucide-svelte'
    	        Import type { ComponentType } from 'svelte'
    	        Import { page } from '$app/stores'
    	        Import logo from '$assets/Spotify_White.Logo.png'
    
              Export let desktop: boolean
    
              Defined a const variable of array `menuItems`
               ↳ Path to `Home`
               ↳ Path to `Search`
               ↳ Path to `Playlists`
    
              Added <div class="nav-content" ... >
                ↳ Added <nav aria-label="Main">
                  ↳ Added <div class="nav-content-inner">
                    ↳ Added <img> of 'Spotify'
                    ↳ Added <ul>
    
              In the <ul>
                ↳ Added an {#each menuItems as item} block
                  ↳ Added <li class:active={item.path === $page.url.pathname}>
                    ↳ Added <a href={item.path}>
                      ↳ Added <svelte:component this={item.icon} size={26} />
                      ↳ Added {item.label}
    
    Modified:   Export { default as Navigation } from './Navigation.svelte' (src/lib/components/index.ts)

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Home, Search, ListMusic, type Icon } from 'lucide-svelte';
+	import type { ComponentType } from 'svelte';
+	import { page } from '$app/stores';
+	import logo from '$assets/Spotify_White.Logo.png';
+
+	export let desktop: boolean;
+
+	const menuItems: { path: string; label: string; icon: ComponentType<Icon> }[] = [
+		{
+			path: '/',
+			label: 'Home',
+			icon: Home
+		},
+		{
+			path: '/search',
+			label: 'Search',
+			icon: Search
+		},
+		{
+			path: '/playlists',
+			label: 'Playlists',
+			icon: ListMusic
+		}
+	];
+</script>
+
+<div class="nav-content" class:desktop class:mobile={!desktop}>
+	<nav aria-label="Main">
+		<div class="nav-content-inner">
+			<img src={logo} class="logo" alt="Spotify" width="100px" />
+			<ul>
+				{#each menuItems as item}
+					<li class:active={item.path === $page.url.pathname}>
+						<a href={item.path}>
+							<svelte:component this={item.icon} size={26} />
+							{item.label}
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</nav>
+</div>
