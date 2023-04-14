ee70c15b57bbcd411e3e1b4e8c69571ea126a9cd
GrungeElFz <elf0114_@hotmail.com>
2023-04-13 22:20:03 -0500

    Modified:   Import logo from '$assets/Spotify_White.Logo.png' (src/routes/login/+page.svelte)             Import cover from '$assets/PFP_01000111.N.png'
                Removed 2 <Button> components
    
                Added <div class="wrapper">
    
                  ↳ Added <div class="content">
                    ↳ Added <img src={logo} class="logo" alt="Spotify" />
                    ↳ Added <h1> with placeholder text
                    ↳ Added <p> with placeholder text
                    ↳ Added <div class="buttons">
                      ↳ Added 2 <Button> components
    
                  ↳ Added <div class="cover">
                    ↳ Added <img src={cover} alt="0x01000111 artist cover" />

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
 	import { Button } from '$components';
+	import logo from '$assets/Spotify_White.Logo.png';
+	import cover from '$assets/PFP_01000111.N.png';
 </script>
 
-<Button element="a" variant="outline" href="">Login</Button>
-<Button element="button" disabled>Login</Button>
+<div class="wrapper">
+	<div class="content">
+		<img src={logo} class="logo" alt="Spotify" />
+		<h1>This is H1 tags</h1>
+		<p>This is P tags, This is P tags, This is P tags.</p>
+		<div class="buttons">
+			<Button
+				element="a"
+				variant="outline"
+				href="https://www.spotify.com/"
+				target="_blank"
+				rel="noopener noreferrer">Sign Up</Button
+			>
+			<Button element="a" href="api/auth/login">Login</Button>
+		</div>
+	</div>
+	<div class="cover">
+		<img src={cover} alt="0x01000111 artist cover" />
+	</div>
+</div>
