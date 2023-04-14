64a95fef231e4f09972f23a3da20f1ada866aa1f
GrungeElFz <elf0114_@hotmail.com>
2023-04-13 23:02:07 -0500

    Stylized:   Added `<style lang="scss">` (src/routes/login/+page.svelte)
                Added 4 outer selectors
    
                {
    
                Added :global(body)
                  ↳ background-image: linear-gradient(230deg, var(--sidebar-color), var(--bg-color))
                  ↳ height: 100vh
    
                Added .wrapper selector
                  ↳ display: flex
                  ↳ width: 100%
                  ↳ max-width: 1200px
                  ↳ margin: 80px auto 0
                  ↳ align-items: center
    
                Added .cover selector
                  ↳ flex: 1
                  ↳ margin-left: 20px
                  ↳ Added img selector
                    ↳ max-width: 100%
    
                Added .content selector
                  ↳ flex: 1.8
                  ↳ Added h1 selector
                    ↳ font-size: functions.toREM(40)
                  ↳ Added p selector
                    ↳ font-size: functions.toREM(20)
                  ↳ Added .buttons selector
                    ↳ margin-top: 40px
                    ↳ Added :global(.button) selector
                      ↳ margin-right: 10px
                      ↳ padding: 10px 10px
                      ↳ text-transform: uppercase
                  ↳ Added .logo selector
                    ↳ width: 170px
                    ↳ max-width: 100%
                    ↳ margin-bottom: 30px
    
                }

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -24,3 +24,46 @@
 		<img src={cover} alt="0x01000111 artist cover" />
 	</div>
 </div>
+
+<style lang="scss">
+	:global(body) {
+		background-image: linear-gradient(230deg, var(--sidebar-color), var(--bg-color));
+		height: 100vh;
+	}
+	.wrapper {
+		display: flex;
+		width: 100%;
+		max-width: 1200px;
+		margin: 80px auto 0;
+		align-items: center;
+	}
+	.cover {
+		flex: 1;
+		margin-left: 20px;
+		img {
+			max-width: 100%;
+		}
+	}
+	.content {
+		flex: 1.8;
+		h1 {
+			font-size: functions.toREM(40);
+		}
+		p {
+			font-size: functions.toREM(20);
+		}
+		.buttons {
+			margin-top: 40px;
+			:global(.button) {
+				margin-right: 10px;
+				padding: 10px 10px;
+				text-transform: uppercase;
+			}
+		}
+		.logo {
+			width: 170px;
+			max-width: 100%;
+			margin-bottom: 30px;
+		}
+	}
+</style>
