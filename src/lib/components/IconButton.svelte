477ae092b672772c71a35e15a96b773f80412a15
GrungeElFz <elf0114_@hotmail.com>
2023-04-23 23:44:26 -0500

    Stylized:		Added `<style lang="scss">` (src/lib/components/IconButton.svelte) 						↳ Added `button` selector 							↳ border: none 							↳ background: none 							↳ cursor: pointer 							↳ padding: 0 							↳ width: 38px 							↳ height: 38px 							↳ Added `:global(svg)` selector 								↳ vertical-align: middle 							↳ Added `&:disabled` selector 								↳ opacity: 0.6 								↳ cursor: not-allowed 							↳ Added `&:active` selector 								↳ background-color: var(--menu-background-color)

--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -16,3 +16,24 @@
 	<svelte:component this={icon} focusable="false" aria-hidden="true" color="var(--text-color)" />
 	<span class="visually-hidden">{label}</span>
 </button>
+
+<style lang="scss">
+	button {
+		border: none;
+		background: none;
+		cursor: pointer;
+		padding: 0;
+		width: 38px;
+		height: 38px;
+		:global(svg) {
+			vertical-align: middle;
+		}
+		&:disabled {
+			opacity: 0.6;
+			cursor: not-allowed;
+		}
+		&:active {
+			background-color: var(--menu-background-color);
+		}
+	}
+</style>
