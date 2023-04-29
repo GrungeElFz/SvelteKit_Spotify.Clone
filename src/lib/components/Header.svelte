c54fce917cac525cd01fa66c999701e9fc70a85f
GrungeElFz <elf0114_@hotmail.com>
2023-04-28 23:47:16 -0500

    Modified:   Added a `User Menu` button (src/lib/components/Header.svelte)
                Added `lang="ts"` to the main script
                Import { page } from '$app/stores'
                Added a reactive statement `user = $page.data.user`
    
                In <div class="right">
                  ↳ Added <div id="profile-button">
                    ↳ Added <button class="profile-button">
                      ↳ Added an {#if user?.images && user.images.length > 0} block
                        ↳ Added <img src={user.images[0].url} alt="" />

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang="ts">
 	import { browser } from '$app/environment';
 	import { Navigation } from '$components';
+	import { page } from '$app/stores';
+
+	$: user = $page.data.user;
 </script>
 
 <div class="content">
@@ -13,7 +16,15 @@
 			<Navigation desktop={false} />
 		{/if}
 	</div>
-	<div class="right">Right</div>
+	<div class="right">
+		<div id="profile-button">
+			<button class="profile-button">
+				{#if user?.images && user.images.length > 0}
+					<img src={user.images[0].url} alt="" />
+				{/if}
+			</button>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
