a2e2fd5861951031a52a37d55e3257191572cf1b
GrungeElFz <elf0114_@hotmail.com>
2023-05-03 00:30:36 -0500

    Modified:   Added a fetch limit to 10 for both responses (src/routes/+page.ts)
                In `const response = await fetch(..)`
                  ↳ Replaced `fetch('/api/spotify/browse/new-releases')`
                  ↳ By `fetch('/api/spotify/browse/new-releases?limit=10')`
    
                In `const response2 = await fetch(..)`
                  ↳ Replaced `fetch('/api/spotify/browse/featured-playlists')`
                  ↳ By `fetch('/api/spotify/browse/featured-playlists?limit=10')`
    
    Modified:   Added a support for query parameters (src/routes/api/spotify/[...path]/+server.ts)
    
                In `export const GET: RequestHandler = async ({ fetch, cookies, params, url }) => {..}`
                ↳ Added an `url` parameter
    
                In `const response = await fetch(`${SPOTIFY_BASE_URL}/${params.path}${url.search}`, {..})`
                ↳ Replaced `${SPOTIFY_BASE_URL}/${params.path}`
                ↳ By `${SPOTIFY_BASE_URL}/${params.path}${url.search}`

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,8 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch('/api/spotify/browse/new-releases');
-	const response2 = await fetch('/api/spotify/browse/featured-playlists');
+	const response = await fetch('/api/spotify/browse/new-releases?limit=10');
+	const response2 = await fetch('/api/spotify/browse/featured-playlists?limit=10');
 
 	console.log(await response.json());
 	console.log(await response2.json());
