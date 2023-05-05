38bb7b3eb7a3e5655945a8be968293cc0c59b401
GrungeElFz <elf0114_@hotmail.com>
2023-05-04 23:08:17 -0500

    Modified:   Added `{ parent }` as a second param for `load` function (src/routes/+page.ts)             ↳ Added `const { user } = await parent()`             ↳ Added `const userPlaylists = fetch('/api/spotify/user/${user?.id}/playlists?limit=10')`             ↳ Added `	const [newReleasesResponse, featuredPlaylistsResponse, userPlaylistsResponse] = await Promise.all(..)`               ↳ `[newReleases, featuredPlaylists, userPlaylists]`             ↳ Added `return {..}`               ↳ `newReleases: newReleasesResponse.ok ? newReleasesResponse.json() : undefined`               ↳ `featuredPlaylists: featuredPlaylistsResponse.ok ? featuredPlaylistsResponse.json() : undefined`               ↳ `userPlaylists: userPlaylistsResponse.ok ? userPlaylistsResponse.json() : undefined`

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,19 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const newReleases = await fetch('/api/spotify/browse/new-releases?limit=10');
-	const featuredPlaylists = await fetch('/api/spotify/browse/featured-playlists?limit=10');
+export const load: PageLoad = async ({ fetch, parent }) => {
+	const { user } = await parent();
+
+	const newReleases = fetch('/api/spotify/browse/new-releases?limit=10');
+	const featuredPlaylists = fetch('/api/spotify/browse/featured-playlists?limit=10');
+	const userPlaylists = fetch('/api/spotify/user/${user?.id}/playlists?limit=10');
+
+	const [newReleasesResponse, featuredPlaylistsResponse, userPlaylistsResponse] = await Promise.all(
+		[newReleases, featuredPlaylists, userPlaylists]
+	);
+
+	return {
+		newReleases: newReleasesResponse.ok ? newReleasesResponse.json() : undefined,
+		featuredPlaylists: featuredPlaylistsResponse.ok ? featuredPlaylistsResponse.json() : undefined,
+		userPlaylists: userPlaylistsResponse.ok ? userPlaylistsResponse.json() : undefined
+	};
 };
