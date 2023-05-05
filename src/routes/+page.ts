fb1295c131dea3d4c33e45fa5ab3a7b3adb73ed0
GrungeElFz <elf0114_@hotmail.com>
2023-05-04 22:56:56 -0500

    Refactored:   Replaced `response` with `newReleases` (src/routes/+page.ts)               Replaced `response2` with `featuredPlaylists`
                  Removed all `console.log(..)`

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,6 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch('/api/spotify/browse/new-releases?limit=10');
-	const response2 = await fetch('/api/spotify/browse/featured-playlists?limit=10');
-
-	console.log(await response.json());
-	console.log(await response2.json());
+	const newReleases = await fetch('/api/spotify/browse/new-releases?limit=10');
+	const featuredPlaylists = await fetch('/api/spotify/browse/featured-playlists?limit=10');
 };
