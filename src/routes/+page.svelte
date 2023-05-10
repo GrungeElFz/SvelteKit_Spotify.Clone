1255899a3d6e870521f4b099795a4b297fa3b472
GrungeElFz <elf0114_@hotmail.com>
2023-05-09 22:21:21 -0500

    Modified:   Home Page Markup (src/routes/+page.svelte)
                Added `let section: {..}[] = []`
                ↳ title: string
                ↳ path: string
                ↳ items: (SpotifyApi.AlbumObjectSimplified | SpotifyApi.PlaylistObjectSimplified)[]
    
                Added a reactive statement
                ↳ if (data.newReleases)
                  ↳ section.push({..})
                    ↳ title: 'New Releases'
                    ↳ path: '/sections/new-releases'
                    ↳ items: data.newReleases.albums.items
    
                ↳ if (data.featuredPlaylists)
                  ↳ section.push({..})
                    ↳ title: 'Featured Playlists'
                    ↳ path: '/section/featured-playlists'
                    ↳ items: data.featuredPlaylists.playlists.items
    
                ↳ if (data.userPlaylists)
                  ↳ section.push({..})
                    ↳ title: 'Your Playlists'
                    ↳ path: '/playlists'
                    ↳ items: data.userPlaylists.items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
+	import { select_options } from 'svelte/internal';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	let section: {
+		title: string;
+		path: string;
+		items: (SpotifyApi.AlbumObjectSimplified | SpotifyApi.PlaylistObjectSimplified)[];
+	}[] = [];
+
+	$: {
+		if (data.newReleases) {
+			section.push({
+				title: 'New Releases',
+				path: '/sections/new-releases',
+				items: data.newReleases.albums.items
+			});
+		}
+		if (data.featuredPlaylists) {
+			section.push({
+				title: 'Featured Playlists',
+				path: '/section/featured-playlists',
+				items: data.featuredPlaylists.playlists.items
+			});
+		}
+		if (data.userPlaylists) {
+			section.push({
+				title: 'Your Playlists',
+				path: '/playlists',
+				items: data.userPlaylists.items
+			});
+		}
+	}
 </script>
 
 <h1>Home</h1>
