51767a24bb0a83eb3720443821e1d8b44c0303db
GrungeElFz <elf0114_@hotmail.com>
2023-05-09 22:39:02 -0500

    Refactored:   Changed all the occurrences `section` -> `sections` (src/routes/+page.svelte)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 	export let data: PageData;
 
-	let section: {
+	let sections: {
 		title: string;
 		path: string;
 		items: (SpotifyApi.AlbumObjectSimplified | SpotifyApi.PlaylistObjectSimplified)[];
@@ -12,23 +12,23 @@
 
 	$: {
 		if (data.newReleases) {
-			section.push({
+			sections.push({
 				title: 'New Releases',
 				path: '/sections/new-releases',
 				items: data.newReleases.albums.items
 			});
 		}
 		if (data.featuredPlaylists) {
-			section.push({
+			sections.push({
 				title: 'Featured Playlists',
-				path: '/section/featured-playlists',
+				path: '/sections/featured-playlists',
 				items: data.featuredPlaylists.playlists.items
 			});
 		}
 		data.homeCategories.forEach((category, index) => {
 			const categoryPlaylist = data.categoriesPlaylists[index];
 			if (categoryPlaylist) {
-				section.push({
+				sections.push({
 					title: category.name,
 					path: `/category/${category.id}`,
 					items: categoryPlaylist.playlists.items
@@ -36,7 +36,7 @@
 			}
 		});
 		if (data.userPlaylists) {
-			section.push({
+			sections.push({
 				title: 'Your Playlists',
 				path: '/playlists',
 				items: data.userPlaylists.items
