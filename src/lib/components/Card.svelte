7f2c84f3b8d62e7b80b61acdacc6165a8332a0b2
GrungeElFz <elf0114_@hotmail.com>
2023-05-10 21:43:35 -0500

    Added:    A Card Component (src/lib/components/Card.svelte)
              Added `<script lang="ts">`
                ↳ type AlbumType = SpotifyApi.AlbumObjectFull | SpotifyApi.AlbumObjectSimplified
                ↳ type PlaylistsType = SpotifyApi.PlaylistObjectFull | SpotifyApi.PlaylistObjectSimplified
                ↳ type ArtistType = SpotifyApi.ArtistObjectFull | SpotifyApi.ArtistObjectSimplified
                ↳ export let item: AlbumType | PlaylistsType | ArtistType
    
    Modified:   Added `export { default as Card } from './Card.svelte'` (src/lib/components/index.ts)

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	type AlbumType = SpotifyApi.AlbumObjectFull | SpotifyApi.AlbumObjectSimplified;
+	type PlaylistsType = SpotifyApi.PlaylistObjectFull | SpotifyApi.PlaylistObjectSimplified;
+	type ArtistType = SpotifyApi.ArtistObjectFull | SpotifyApi.ArtistObjectSimplified;
+
+	export let item: AlbumType | PlaylistsType | ArtistType;
+</script>
