43f2cedba582ba5c14c5b53e5144c00426088a38
GrungeElFz <elf0114_@hotmail.com>
2023-05-11 07:08:39 -0500

    Modified:   Added `<div class="card {item.type}>` (src/lib/components/Card.svelte)               ↳ {#if item.images.length > o}                 ↳ <img loading="lazy" src={item.images[0].url} alt="{item.type} cover for {item.name}" />
                Removed `SpotifyApi.ArtistObjectSimplified` from `type ArtistType`

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	type AlbumType = SpotifyApi.AlbumObjectFull | SpotifyApi.AlbumObjectSimplified;
 	type PlaylistsType = SpotifyApi.PlaylistObjectFull | SpotifyApi.PlaylistObjectSimplified;
-	type ArtistType = SpotifyApi.ArtistObjectFull | SpotifyApi.ArtistObjectSimplified;
+	type ArtistType = SpotifyApi.ArtistObjectFull;
 
 	export let item: AlbumType | PlaylistsType | ArtistType;
 </script>
+
+<div class="card {item.type}">
+	{#if item.images.length > 0}
+		<img loading="lazy" src={item.images[0].url} alt="{item.type} cover for {item.name}" />
+	{/if}
+</div>
