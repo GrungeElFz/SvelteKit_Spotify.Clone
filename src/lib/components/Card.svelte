9108cba0df5e6f3875f9df925fb7e5281861ec4b
GrungeElFz <elf0114_@hotmail.com>
2023-05-11 07:42:59 -0500

    Stylized:   Added `:global(svg)` selector (src/lib/components/Card.svelte)               ↳ width: 40%               ↳ height: 40%
                In `<Music aria-hidden="true" focusable="false" />`
                ↳ Added an attribute `color="var(--light-grey)"`

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -13,7 +13,7 @@
 		<img loading="lazy" src={item.images[0].url} alt="{item.type} cover for {item.name}" />
 	{:else}
 		<div class="cover-placeholder">
-			<Music aria-hidden="true" focusable="false" />
+			<Music aria-hidden="true" focusable="false" color="var(--light-grey)" />
 		</div>
 	{/if}
 </div>
@@ -40,6 +40,10 @@
 			background-color: var(--medium-grey);
 			margin: 0 0 20px;
 			transition: background 0.3s;
+			:global(svg) {
+				width: 40%;
+				height: 40%;
+			}
 		}
 	}
 </style>
