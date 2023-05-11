2bc8e695500e8158751a807119e9542e70048552
GrungeElFz <elf0114_@hotmail.com>
2023-05-11 07:14:06 -0500

    Modified:   Import { Music } from 'lucide-svelte' (src/lib/components/Card.svelte)
                In <div class="card {item.type}">
                  ↳ Added an {:else} block
                    ↳ Added <div class="cover-placeholder">
                      ↳ Added <Music aria-hidden="true" focusable="false" />

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { Music } from 'lucide-svelte';
+
 	type AlbumType = SpotifyApi.AlbumObjectFull | SpotifyApi.AlbumObjectSimplified;
 	type PlaylistsType = SpotifyApi.PlaylistObjectFull | SpotifyApi.PlaylistObjectSimplified;
 	type ArtistType = SpotifyApi.ArtistObjectFull;
@@ -9,5 +11,9 @@
 <div class="card {item.type}">
 	{#if item.images.length > 0}
 		<img loading="lazy" src={item.images[0].url} alt="{item.type} cover for {item.name}" />
+	{:else}
+		<div class="cover-placeholder">
+			<Music aria-hidden="true" focusable="false" />
+		</div>
 	{/if}
 </div>
