9f33dc54b4cff0ce0a5d0a02a905304000a6eec0
GrungeElFz <elf0114_@hotmail.com>
2023-05-12 14:46:48 -0500

    Modified:   Added `<h4><a href="/{item.type}/{item.id}">{item.name}</a></h4>` (src/lib/components/Card.svelte)             Added an {#if item.type === 'album'} block               ↳ <p>{item.artists.map((item) => item.name).join(', ')}</p>             Added an {#if item.type === 'playlist'} block               ↳ <p>{item.description}</p>

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -16,6 +16,13 @@
 			<Music aria-hidden="true" focusable="false" color="var(--light-grey)" />
 		</div>
 	{/if}
+	<h4><a href="/{item.type}/{item.id}">{item.name}</a></h4>
+	{#if item.type === 'album'}
+		<p>{item.artists.map((item) => item.name).join(', ')}</p>
+	{/if}
+	{#if item.type === 'playlist'}
+		<p>{item.description}</p>
+	{/if}
 </div>
 
 <style lang="scss">
