5e5638d422b5c66a73aacc3600a1b44f2228ca76
GrungeElFz <elf0114_@hotmail.com>
2023-05-14 15:23:48 -0500

    Added:    Album Page (src/routes/album/[id]/+page.svelte)
              Added <script lang="ts">
                ↳ import type { PageData } from './$types'
                ↳ export let data: PageData
                ↳ $: album = data.album
                ↳ $: console.log(album)
    
              Added <h5>{album.album_type}</h5>
              Added <h1>{album.name}</h1>
    
    Added:    Album Data Loader (src/routes/album/[id]/+page.ts)
    
              import { fetchRefresh } from '$helpers'
              import { error } from '@sveltejs/kit'
              import type { PageLoad } from './$types'
    
              export const load: PageLoad = async ({ fetch, params }) => {..}
                ↳ const albumRes = await fetchRefresh(fetch, `/api/spotify/albums/${params.id}`)
                ↳ if (!albumRes.ok) {..}
                  ↳ throw error(albumRes.status, 'Failed to load album!')
                ↳ const albumJSON: SpotifyApi.SingleAlbumResponse = await albumRes.json()
                ↳ return {..}
                  ↳ album: albumJSON
                  ↳ title: albumJSON.name

---
+++ src/routes/album/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: album = data.album;
+	$: console.log(album);
+</script>
+
+<h5>{album.album_type}</h5>
+<h1>{album.name}</h1>
