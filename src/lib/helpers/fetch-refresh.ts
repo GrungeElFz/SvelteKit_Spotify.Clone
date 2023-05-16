a5c78f9f9f64e5ecbdd811b5b198930c39b696f2
GrungeElFz <elf0114_@hotmail.com>
2023-05-15 22:01:01 -0500

    Debugged:   Fixed the `refreshPromise` warning (src/lib/helpers/fetch-refresh.ts)
                Added `declare global {..}`
                  ↳ interface Window {..}
                    ↳ refreshPromise: Promise<Response> | null
    
                /**
                 * Warning - Property `refreshPromise` does not exist on type 'Window & typeof globalThis'.ts(2339).
                 * Solution - Defined `refreshPromise` as a property on the Window object.
                 */

--- src/lib/helpers/fetch-refresh.ts
+++ src/lib/helpers/fetch-refresh.ts
@@ -1,6 +1,12 @@
 import { browser } from '$app/environment';
 import { error } from '@sveltejs/kit';
 
+declare global {
+	interface Window {
+		refreshPromise: Promise<Response> | null;
+	}
+}
+
 export default async function fetchRefresh(
 	fetch: (input: URL | RequestInfo, init?: RequestInit | undefined) => Promise<Response>,
 	path: string
