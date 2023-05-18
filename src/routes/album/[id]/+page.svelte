4268097f94d4324c1a3cd2bb25aec33f040cc354
GrungeElFz <elf0114_@hotmail.com>
2023-05-17 23:31:14 -0500

    Modified:   Import { ItemPage } from '$components' (src/routes/album/[id]/+page.svelte)
                Removed `<h5>{album.album_type}</h5>`
                Removed `<h1>{album.name}</h1>`
    
                Added `<ItemPage ... >`
                ↳ title={album.name}
                ↳ type={album.album_type}
                ↳ color={null}
                ↳ image={undefined}
    
                  ↳ Encapsulated 2 divs into `<ItemPage ...>`
                    ↳ <div class="tracks">
                    ↳ <div class="credits">

--- src/routes/album/[id]/+page.svelte
+++ src/routes/album/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { getCopyrightSymbol } from '$helpers';
+	import { ItemPage } from '$components';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -8,28 +9,27 @@
 	$: console.log(album);
 </script>
 
-<h5>{album.album_type}</h5>
-<h1>{album.name}</h1>
+<ItemPage title={album.name} type={album.album_type} color={null} image={undefined}>
+	<div class="tracks">
+		<ul>
+			{#each album.tracks.items as track}
+				<li>{track.name}</li>
+			{/each}
+		</ul>
+	</div>
 
-<div class="tracks">
-	<ul>
-		{#each album.tracks.items as track}
-			<li>{track.name}</li>
-		{/each}
-	</ul>
-</div>
-
-<div class="credits">
-	<p class="date">
-		{new Date(album.release_date).toLocaleDateString('en', { dateStyle: 'medium' })}
-	</p>
-	{#each album.copyrights as copyright}
-		<p class="copyright">
-			{getCopyrightSymbol(copyright.type)}
-			{copyright.text}
+	<div class="credits">
+		<p class="date">
+			{new Date(album.release_date).toLocaleDateString('en', { dateStyle: 'medium' })}
 		</p>
-	{/each}
-</div>
+		{#each album.copyrights as copyright}
+			<p class="copyright">
+				{getCopyrightSymbol(copyright.type)}
+				{copyright.text}
+			</p>
+		{/each}
+	</div>
+</ItemPage>
 
 <style lang="scss">
 	.credits {
