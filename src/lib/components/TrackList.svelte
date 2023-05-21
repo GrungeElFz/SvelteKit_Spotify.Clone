685bc51ca66b8d48d4a8116643e4e1012960903a
GrungeElFz <elf0114_@hotmail.com>
2023-05-21 17:40:33 -0500

    Modified:   Import { ListPlus } from 'lucide-svelte' (src/lib/components/TrackList.svelte)
                Added an {#each tracks as track, index} block
                  ↳ <div class="row">
                    ↳ <div class="number-column">
                      ↳ <span class="number">{index + 1}</span>
    
                  ↳ <div class="info-column">
                    ↳ <div class="track-title">
                      ↳ <h4>{track.name}</h4>
                      ↳ {#if track.explicit}
                        ↳ <span class="explicit">Explicit</span>
    
                  ↳ <p class="artists">
                    ↳ {#each track.artists as artist, artistIndex}
                      ↳ <a href="/artist/{artist.id}">{artist.name}</a>
                      ↳ {#if artistIndex < track.artists.length - 1}{', '}{/if}
    
                  ↳ <div class="duration-column">
                    ↳ <span class="duration">{track.duration_ms}</span>
    
                  ↳ <div class="actions-column">
                    ↳ <ListPlus aria-hidden focusable="false" />

--- src/lib/components/TrackList.svelte
+++ src/lib/components/TrackList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Clock8 } from 'lucide-svelte';
+	import { Clock8, ListPlus } from 'lucide-svelte';
 
 	export let tracks: SpotifyApi.TrackObjectFull[] | SpotifyApi.TrackObjectSimplified[];
 </script>
@@ -20,4 +20,36 @@
 
 		<div class="actions-column" />
 	</div>
+
+	{#each tracks as track, index}
+		<div class="row">
+			<div class="number-column">
+				<span class="number">{index + 1}</span>
+			</div>
+
+			<div class="info-column">
+				<div class="track-title">
+					<h4>{track.name}</h4>
+					{#if track.explicit}
+						<span class="explicit">Explicit</span>
+					{/if}
+				</div>
+
+				<p class="artists">
+					{#each track.artists as artist, artistIndex}
+						<a href="/artist/{artist.id}">{artist.name}</a>
+						{#if artistIndex < track.artists.length - 1}{', '}{/if}
+					{/each}
+				</p>
+			</div>
+
+			<div class="duration-column">
+				<span class="duration">{track.duration_ms}</span>
+			</div>
+
+			<div class="actions-column">
+				<ListPlus aria-hidden focusable="false" />
+			</div>
+		</div>
+	{/each}
 </div>
