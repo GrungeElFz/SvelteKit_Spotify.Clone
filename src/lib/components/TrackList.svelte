99ca38da04a71f088a24ceadb01ddf33ed322dfa
GrungeElFz <elf0114_@hotmail.com>
2023-05-21 15:46:21 -0500

    Modified:   Import { Clock8 } from 'lucide-svelte' (src/lib/components/TrackList.svelte)
                Added <div class="tracks">
                  ↳ <div class="row header">
    
                    ↳ <div class="number-column">
                      ↳ <span class="number">#</span>
    
                    ↳ <div class="info-column">
                      ↳ <span class="track-title">Title</span>
    
                    ↳ <div class="duration-column">
                      ↳ <Clock8 aria-hidden focusable="false" />
    
                    ↳ <div class="actions-column" />

--- src/lib/components/TrackList.svelte
+++ src/lib/components/TrackList.svelte
@@ -1,3 +1,23 @@
 <script lang="ts">
+	import { Clock8 } from 'lucide-svelte';
+
 	export let tracks: SpotifyApi.TrackObjectFull[] | SpotifyApi.TrackObjectSimplified[];
 </script>
+
+<div class="tracks">
+	<div class="row header">
+		<div class="number-column">
+			<span class="number">#</span>
+		</div>
+
+		<div class="info-column">
+			<span class="track-title">Title</span>
+		</div>
+
+		<div class="duration-column">
+			<Clock8 aria-hidden focusable="false" />
+		</div>
+
+		<div class="actions-column" />
+	</div>
+</div>
