9aa9c1fa2fd6372fa7d9075a47129995762bd80d
GrungeElFz <elf0114_@hotmail.com>
2023-05-22 18:57:36 -0500

    Added:    An `Audio Player` component (src/lib/components/Player.svelte)
              Added <script lang="ts">
                ↳ type Track = SpotifyApi.TrackObjectFull | SpotifyApi.TrackObjectSimplified;
                ↳ export let track: Track;
                ↳ let audio: HTMLAudioElement;
                ↳ let paused: true;
    
              Added <div class="player">
                ↳ Added <audio bind:this={audio} bind:paused controls src={track.preview_url} preload="none" />
    
    Modified:   Import { Player } from '$components' (src/lib/components/TrackList.svelte)
    
                In <div class="number-column">
                  ↳ Added <div class="player">
                    ↳ <Player {track} />
    
    Modified:   Export { default as Player } from './Player.svelte' (src/lib/components/index.ts)

---
+++ src/lib/components/Player.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	type Track = SpotifyApi.TrackObjectFull | SpotifyApi.TrackObjectSimplified;
+	export let track: Track;
+
+	let audio: HTMLAudioElement;
+	let paused: true;
+</script>
+
+<div class="player">
+	<audio bind:this={audio} bind:paused controls src={track.preview_url} preload="none" />
+</div>
