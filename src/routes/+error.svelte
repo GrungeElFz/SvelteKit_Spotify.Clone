69a8f2590bc0c7573c8327c70e3e6a8a2a4f5a9f
GrungeElFz <elf0114_@hotmail.com>
2023-05-23 12:00:02 -0500

    Added:   An error page for routes (src/routes/+error.svelte)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Button, LogoutButton } from '$components';
+</script>
+
+<svelte:head>
+	<title>{$page.status}</title>
+</svelte:head>
+
+<div class="content">
+	<h1>{$page.error?.message}</h1>
+
+	{#if $page.status === 404}
+		<p>The page you are trying to access cannot be found.</p>
+		<div class="buttons">
+			<Button element="a" href="/">Home</Button>
+			<Button element="a" href="/search">Search</Button>
+		</div>
+	{/if}
+
+	{#if $page.status === 401}
+		<p>Your current session has expired, please logout and login again.</p>
+		<div class="buttons">
+			<LogoutButton />
+		</div>
+	{/if}
+</div>
