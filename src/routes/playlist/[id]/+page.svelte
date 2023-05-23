8ad8c17afb82ed1381a092edcbd0ef9d345fc30c
GrungeElFz <elf0114_@hotmail.com>
2023-05-23 12:50:21 -0500

    Modified:   Playlist Page Markup (src/routes/playlist/[id]/+page.svelte)
                Added `import { Button, ItemPage } from '$components'`
                Added `import TrackList from '$components/TrackList.svelte'`
    
                Added `$: color = data.color`
                Added `$: playlist = data.playlist`
                Added `$: tracks = data.playlist.tracks`
                Added `let filteredTracks: SpotifyApi.TrackObjectFull[]`
                Added `$: {..}`
                  ↳ filteredTracks = []
                  ↳ tracks.items.forEach((item) => {..})
                    ↳ if (item.track) filteredTracks = [...filteredTracks, item.track]
                Added `const followersFormat = Intl.NumberFormat('en', { notation: 'compact' })`
    
                Added <ItemPage ... >
                ↳ title={playlist.name}
                ↳ image={playlist.images.length > 0 ? playlist.images[0].url : undefined}
                ↳ {color}
                ↳ type={playlist.type}
    
                  ↳	<div slot="meta">
                    ↳ <p class="playlist-description">{@html playlist.description}</p>
                    ↳ <p class="meta">
                      ↳ <span>{playlist.owner.display_name}</span>
                      ↳ <span>{followersFormat.format(playlist.followers.total)}</span>
                      ↳ <span>{playlist.tracks.total} Tracks</span>
    
                  ↳ {#if playlist.tracks.items.length > 0}
                    ↳ <TrackList tracks={filteredTracks} />
                  ↳ {:else}
                    ↳ <div class="empty-playlist">
                      ↳ <p>No items added to this playlist yet.</p>
                      ↳ <Button element="a" href="/search">Search for Content</Button>
                      ↳ <Button element="a" href="/playlists">View all Playlists</Button>

--- src/routes/playlist/[id]/+page.svelte
+++ src/routes/playlist/[id]/+page.svelte
@@ -1,7 +1,49 @@
 <script lang="ts">
+	import { Button, ItemPage } from '$components';
+	import TrackList from '$components/TrackList.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
 	$: console.log(data);
+	$: color = data.color;
+	$: playlist = data.playlist;
+	$: tracks = data.playlist.tracks;
+
+	let filteredTracks: SpotifyApi.TrackObjectFull[];
+
+	$: {
+		filteredTracks = [];
+		tracks.items.forEach((item) => {
+			if (item.track) filteredTracks = [...filteredTracks, item.track];
+		});
+	}
+
+	const followersFormat = Intl.NumberFormat('en', { notation: 'compact' });
 </script>
+
+<ItemPage
+	title={playlist.name}
+	image={playlist.images.length > 0 ? playlist.images[0].url : undefined}
+	{color}
+	type={playlist.type}
+>
+	<div slot="meta">
+		<p class="playlist-description">{@html playlist.description}</p>
+		<p class="meta">
+			<span>{playlist.owner.display_name}</span>
+			<span>{followersFormat.format(playlist.followers.total)}</span>
+			<span>{playlist.tracks.total} Tracks</span>
+		</p>
+	</div>
+
+	{#if playlist.tracks.items.length > 0}
+		<TrackList tracks={filteredTracks} />
+	{:else}
+		<div class="empty-playlist">
+			<p>No items added to this playlist yet.</p>
+			<Button element="a" href="/search">Search for Content</Button>
+			<Button element="a" href="/playlists">View all Playlists</Button>
+		</div>
+	{/if}
+</ItemPage>
