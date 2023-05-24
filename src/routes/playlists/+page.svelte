ed994f7d694634c8fdc6c8ea33f4661f2dbde065
GrungeElFz <elf0114_@hotmail.com>
2023-05-23 23:27:13 -0500

    Modified:   Case handling for users with no playlist (src/routes/playlists/+page.svelte)
                In `<div class="content">`
                  ↳ Added `{#if playlists.items.length > 0}`
                    ↳ Encapsulated 	`<div class="title">` and `<div class="grid-items">` into here
                  ↳ Added `{:else}`
                    ↳ <div class="empty">
                      ↳ <p>No Playlists Yet!</p>
                      ↳ <Button element="a">+ Add New</Button>

--- src/routes/playlists/+page.svelte
+++ src/routes/playlists/+page.svelte
@@ -8,14 +8,21 @@
 </script>
 
 <div class="content">
-	<div class="title">
-		<h1>{data.title}</h1>
-		<Button element="a">+ Add New</Button>
-	</div>
+	{#if playlists.items.length > 0}
+		<div class="title">
+			<h1>{data.title}</h1>
+			<Button element="a">+ Add New</Button>
+		</div>
 
-	<div class="grid-items">
-		{#each playlists.items as item}
-			<Card {item} />
-		{/each}
-	</div>
+		<div class="grid-items">
+			{#each playlists.items as item}
+				<Card {item} />
+			{/each}
+		</div>
+	{:else}
+		<div class="empty">
+			<p>No Playlists Yet!</p>
+			<Button element="a">+ Add New</Button>
+		</div>
+	{/if}
 </div>
