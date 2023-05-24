b82a1bf0ac21d74e4a4ef5fb2b680a0274e43afb
GrungeElFz <elf0114_@hotmail.com>
2023-05-23 23:16:31 -0500

    Modified:   `Playlists` Markup (src/routes/playlists/+page.svelte)
                Removed `<h1>Playlists</h1>`
    
                Added `<script lang="ts">`
                  ↳ import { Button, Card } from '$components'
                  ↳ import type { PageData } from './$types'
                  ↳ export let data: PageData
                  ↳ $: playlists = data.userPlaylists
    
                Added `<div class="content">`
                  ↳ <div class="title">
                    ↳ <h1>{data.title}</h1>
                    ↳ <Button element="a">+ Add New</Button>
    
                  ↳ <div class="grid-items">
                    ↳ {#each playlists.items as item}
                      ↳ <Card {item} />
    
    Modified:   import { fetchRefresh } from '$helpers` (src/routes/playlists/+page.ts)
                import { error } from '@sveltejs/kit'
    
                In `export const load: PageLoad = () => {..}`
                ↳ Replaced `()`
                ↳ By `async ({ fetch, url })`
    
                In `export const load: PageLoad = async ({ fetch, url }) => {..}`
                  ↳ Added `const limit = 18`
                  ↳ Added `const page = url.searchParams.get('page')`
    
                  ↳ Added `const searchParams = new URLSearchParams({..}).toString()`
                    ↳ limit: `${limit}`
                    ↳ offset: page ? `${limit * (Number(page) - 1)}` : '0'
    
                  ↳ Added `const playlistsRes = await fetchRefresh(fetch, `/api/spotify/me/playlists?${searchParams}`)`
    
                  ↳ Added `if (!playlistsRes.ok) {..}`
                    ↳ throw error(playlistsRes.status, 'Failed to load playlists!')
    
                  ↳ Added `const playlistsJSON: SpotifyApi.ListOfCurrentUsersPlaylistsResponse = await playlistsRes.json()`
    
                  ↳ In `return {..}`
                    ↳ Added `userPlaylists: playlistsJSON`
                    ↳ Replaced `title: 'Playlists'
                    ↳ By `title: 'Your Playlists'`

--- src/routes/playlists/+page.svelte
+++ src/routes/playlists/+page.svelte
@@ -1,1 +1,21 @@
-<h1>Playlists</h1>
+<script lang="ts">
+	import { Button, Card } from '$components';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: playlists = data.userPlaylists;
+</script>
+
+<div class="content">
+	<div class="title">
+		<h1>{data.title}</h1>
+		<Button element="a">+ Add New</Button>
+	</div>
+
+	<div class="grid-items">
+		{#each playlists.items as item}
+			<Card {item} />
+		{/each}
+	</div>
+</div>
