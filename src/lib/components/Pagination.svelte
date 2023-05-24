02acf93876889aa00fc404ba5d50a882e2219864
GrungeElFz <elf0114_@hotmail.com>
2023-05-24 00:18:20 -0500

    Modified:   Replaced a placeholder with a functional code (src/lib/components/Pagination.svelte)
    Modified:   Added `import { toasts } from '$stores'` (src/routes/playlists/+page.svelte)
                Added `let isLoading = false`
    
                Added `async function loadMoreItems() {..}`
                  ↳ if (!playlists.next) return
                  ↳ isLoading = true
                  ↳ const res = await fetch(playlists.next.replace('https://api.spotify.com/v1/', '/api/spotify/'))
                  ↳ if (res.ok) {..}
                    ↳ const resJSON = await res.json()
                    ↳ playlists = {..}
                      ↳ ...resJSON
                      ↳ items: [...playlists.items, ...resJSON.items]
                    ↳ else {..}
                      ↳ toasts.error('Could not load data!')
                    ↳ isLoading = false
    
                In `<Pagination />
                ↳ Added `paginatedList={playlists} on:loadmore={loadMoreItems} {isLoading}`

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -1,1 +1,56 @@
-<h4>Pagination</h4>
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Button } from '$components';
+	import { createEventDispatcher } from 'svelte';
+
+	export let paginatedList: SpotifyApi.PagingObject<any>;
+	export let isLoading: boolean;
+
+	$: currentPage = $page.url.searchParams.get('page') || 1;
+
+	const dispatch = createEventDispatcher<{
+		loadmore: {};
+	}>();
+</script>
+
+{#if paginatedList.next}
+	<div class="load-more">
+		<Button
+			element="button"
+			variant="outline"
+			disabled={isLoading}
+			on:click={() => dispatch('loadmore')}
+		>
+			Load More <span class="visually-hidden">Items</span>
+		</Button>
+	</div>
+{/if}
+
+<div class="pagination">
+	<div class="previous">
+		{#if paginatedList.previous}
+			<Button
+				element="a"
+				variant="outline"
+				href="{$page.url.pathname}?{new URLSearchParams({
+					page: `${Number(currentPage) - 1}`
+				})}"
+			>
+				← Previous Page
+			</Button>
+		{/if}
+	</div>
+	<div class="next">
+		{#if paginatedList.next}
+			<Button
+				element="a"
+				variant="outline"
+				href="{$page.url.pathname}?{new URLSearchParams({
+					page: `${Number(currentPage) + 1}`
+				})}"
+			>
+				Next Page →
+			</Button>
+		{/if}
+	</div>
+</div>
