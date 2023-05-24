81d3b7f2631209076617a8909c50502ce65b6fdc
GrungeElFz <elf0114_@hotmail.com>
2023-05-24 18:44:14 -0500

    Added:    Playlist load function (src/routes/playlist/[id]/edit/+page.ts)
    Modified:   Pasted the props (src/routes/playlist/[id]/edit/+page.svelte)
    
    Modified:   Defined the props (src/lib/components/PlaylistForm.svelte)

--- src/routes/playlist/[id]/edit/+page.svelte
+++ src/routes/playlist/[id]/edit/+page.svelte
@@ -1,1 +1,13 @@
-<h2>Edit Playlist</h2>
+<script lang="ts">
+	import { PlaylistForm } from '$components';
+	import type { ActionData, PageData } from './$types';
+
+	export let form: ActionData;
+	export let data: PageData;
+
+	$: playlist = data.playlist;
+</script>
+
+<h2>{data.title}</h2>
+
+<PlaylistForm {form} {playlist} action="?redirect" />
