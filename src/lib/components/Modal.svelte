97643fba3933a8f5ebd548272754562cb3a721bc
GrungeElFz <elf0114_@hotmail.com>
2023-05-24 19:12:09 -0500

    Added:    `Modal` component (src/lib/components/Modal.svelte)
    Modified:   Added `export { default as Modal } from './Modal.svelte'` (src/lib/components/index.ts)

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { X } from 'lucide-svelte';
+
+	export let id: string;
+	export let title: string;
+</script>
+
+<div {id} aria-hidden="true" class="modal">
+	<div tabindex="-1" data-micromodal-close class="modal-overlay">
+		<div class="modal-content" role="dialog" aria-modal="true" aria-labelledby="{id}-title">
+			<!-- ↓ header ↓ -->
+			<header>
+				<h2 id="{id}-title">{title}</h2>
+				<button class="close-button" aria-label="Close modal" data-micromodal-close>
+					<X aria-hidden focusable="false" />
+				</button>
+			</header>
+			<!-- ↑ header↑  -->
+			<div id="{id}-content">
+				<slot />
+			</div>
+		</div>
+	</div>
+</div>
