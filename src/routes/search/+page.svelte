78019a03d3103185a7387aee3ff66c77d6c12a51
GrungeElFz <elf0114_@hotmail.com>
2023-05-25 14:39:53 -0500

    Modified:   `Search Page` component (src/routes/search/+page.svelte)
    Modified:   `Search Page` load (src/routes/search/+page.ts)

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,1 +1,22 @@
-<h1>Search</h1>
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	$: items = data.categories?.categories.items;
+</script>
+
+{#if items}
+	<h2>Browse Categories</h2>
+	<div class="items">
+		{#each items as item}
+			<div class="item">
+				<a href="/category/{item.id}">
+					{#if item.icons.length > 0}
+						<img src={item.icons[0].url} alt="" />
+					{/if}
+					<span class="truncate-1">{item.name}</span>
+				</a>
+			</div>
+		{/each}
+	</div>
+{/if}
