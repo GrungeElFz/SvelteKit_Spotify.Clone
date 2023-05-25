a15ce6d6ea18568611ab3cdc870fa62a9ae03131
GrungeElFz <elf0114_@hotmail.com>
2023-05-25 13:25:59 -0500

    Modified:   `Profile Page` Markup (src/routes/profile/+page.svelte)
    Added:    `Profile Page` load (src/routes/profile/+page.ts)

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Profile</h1>
+<script lang="ts">
+	import ItemPage from '$components/ItemPage.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	$: user = data.user;
+	$: color = data.color;
+
+	const followersFormat = Intl.NumberFormat('en', { notation: 'compact' });
+</script>
+
+<div id="profile-page">
+	<ItemPage
+		{color}
+		title={user?.display_name || ''}
+		image={user?.images && user?.images?.length > 0 ? user.images[0].url : undefined}
+		type={user?.type}
+	>
+		<span slot="meta" class="following-count"
+			>{followersFormat.format(user?.followers?.total || 0)} Followers</span
+		>
+	</ItemPage>
+</div>
